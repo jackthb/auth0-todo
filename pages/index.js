70623141a4f813e60a444aecac7388f05dae2b27
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-03-14 18:33:40 +0000

    fixed index

--- pages/index.js
+++ pages/index.js
@@ -4,55 +4,55 @@ import Todo from '../components/Todo';
 import { table, minifyRecords } from './api/utils/Airtable';
 import { TodosContext } from '../context/TodosContext';
 import { useContext, useEffect } from 'react';
-import { getSession, useUser } from '@auth0/nextjs-auth0';
+import { useUser, getSession } from '@auth0/nextjs-auth0';
 import TodoForm from '../components/TodoForm';
 
-export default function Home({ initialTodos }) {
+export default function Home({ initialTodos, user }) {
   const { todos, setTodos } = useContext(TodosContext);
-  const { user, error, isLoading } = useUser();
   useEffect(() => {
     setTodos(initialTodos);
   }, []);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  return (
+    <div>
+      <Head>
+        <title>auth0-todo</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-  if (user) {
-    return (
-      <div>
-        <Head>
-          <title>auth0-todo</title>
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-
-        <main>
-          <Navbar user={user} />
-          {user && (
-            <>
-              <h1 className='text-2xl text-center mb-4'>My Todos</h1>
-              <TodoForm>
-                <ul>
-                  {todos &&
-                    todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
-                </ul>
-              </TodoForm>
-            </>
-          )}
-        </main>
-      </div>
-    );
-  }
-  return <a href='/api/auth/login'>Login</a>;
+      <main>
+        <Navbar user={user} />
+        {user && (
+          <>
+            <TodoForm>
+              <ul>
+                {todos &&
+                  todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+              </ul>
+            </TodoForm>
+          </>
+        )}
+        {!user && <p>Login to save todos!</p>}
+      </main>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-  // const session = await getSession(context.req);
+  const session = await getSession(context.req, context.res);
+  let todos = [];
   try {
-    const todos = await table.select({}).firstPage();
+    if (session?.user) {
+      todos = await table
+        .select({
+          filterByFormula: `userId = '${session.user.sub}'`,
+        })
+        .firstPage();
+    }
     return {
       props: {
         initialTodos: minifyRecords(todos),
-        // user: session?.user || null,
+        user: session?.user || null,
       },
     };
   } catch (err) {
