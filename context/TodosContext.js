7ac5d1c9d7d5c1d2102a886c1334799a6b77a023
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-03-14 13:01:27 +0000

    todo context

--- context/TodosContext.js
+++ context/TodosContext.js
@@ -19,43 +19,47 @@ const TodosProvider = ({ children }) => {
       const res = await fetch('/api/createTodo', {
         method: 'POST',
         body: JSON.stringify({ description }),
-        headers={'Content-Type': 'application/json'}
+        headers: { 'Content-Type': 'application/json' },
       });
       const newTodo = await res.json();
       setTodos((prevTodos) => {
-        return [newTodo, ...prevTodos]
+        return [newTodo, ...prevTodos];
       });
     } catch (error) {
       console.error(error);
     }
   };
-  const updateTodo = async (description) => {
+  const updateTodo = async (updatedTodo) => {
     try {
-      const res = await fetch('/api/updateTodo', {
+      await fetch('/api/updateTodo', {
         method: 'PUT',
-        body: JSON.stringify(updateTodo),
-        headers={'Content-Type': 'application/json'}
+        body: JSON.stringify(updatedTodo),
+        headers: {
+          'content-type': 'application/json',
+        },
       });
-      await res.json();
+
       setTodos((prevTodos) => {
-        const existingTodos = [...prevTodos]
-        const existingTodo = existingTodos.find(todo => todo.id === updateTodo.id)
-        existingTodo.fields = updatedTodo.fields
-        return existingTodos
+        const existingTodos = [...prevTodos];
+        const existingTodo = existingTodos.find(
+          (todo) => todo.id === updatedTodo.id
+        );
+        existingTodo.fields = updatedTodo.fields;
+        return existingTodos;
       });
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.error(err);
     }
   };
   const deleteTodo = async (id) => {
     try {
       const res = await fetch('/api/deleteTodo', {
         method: 'DELETE',
-        body: JSON.stringify({id}),
-        headers={'Content-Type': 'application/json'}
+        body: JSON.stringify({ id }),
+        headers: { 'Content-Type': 'application/json' },
       });
       setTodos((prevTodos) => {
-        return prevTodos.filter((todo) => todo.id != id)
+        return prevTodos.filter((todo) => todo.id != id);
       });
     } catch (error) {
       console.error(error);
@@ -76,3 +80,5 @@ const TodosProvider = ({ children }) => {
     </TodosContext.Provider>
   );
 };
+
+export { TodosProvider, TodosContext };
